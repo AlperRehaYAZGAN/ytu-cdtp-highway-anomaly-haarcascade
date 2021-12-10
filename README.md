343051db0f9458360fc8727b597f68991d074a76
Alper Reha Yazgan <alperreha@gmail.com>
2021-12-10 15:33:01 +0300

    exit highway anomaly added

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # YTU CDTP Highway Anomaly Detection
 This project aiming to detect anomly from highway road cameras.
 
+This project uses venv for manage python virtual environment so follow this guideline.  
+
+# 1-Install venv and Create venv
+- https://docs.python.org/3/library/venv.html  
+- pip install virtualenv
+- python -m venv venv
+- (Windows) .\venv\Scripts\activate
+- (Linux) ./venv/scripts/activate
+- You are ready to go!!!
+
+# 2-Usage
+- Go to project directory via activated venv cli
+- pip install -r requirements.txt
+- python app.py 
+- You will see output like this.  
+
+![car-detection-sample](./docs/sample-output-1.jpg)
--- app.py
+++ app.py
@@ -12,12 +12,94 @@
 #
 #   APP INTERNAL DEPENDENCIES
 #
-import time 
 import os
-import json
 # dotenv
 from dotenv import load_dotenv
 load_dotenv()
 
-# dotenv initial values
-CAMERA_STREAM_URLS = os.getenv("RASP_CAMERA_URLS", "http://127.0.0.1:5000/stream").split(",")
+#
+#   OPENCV DEPENDENCIES
+#
+
+import cv2
+# print opencv version
+print("OpenCV version: {}".format(cv2.__version__))
+
+# path to video and cascade model
+cascade_src = os.path.join(os.getcwd(), 'haarcascades','cars.xml')
+video_src = os.path.join(os.getcwd(), 'samples','highway-nonanomaly.mp4')
+
+# print paths
+print("Cascade path: {}".format(cascade_src))
+print("Video path: {}".format(video_src))
+
+cap = cv2.VideoCapture(video_src)
+car_cascade = cv2.CascadeClassifier(cascade_src)
+
+    
+
+while True:
+    ret, img = cap.read()
+    if (type(img) == type(None)):
+        break
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+
+    # highway line borders
+    # left border is 1/4 of the screen width, right border is 3/4 of the screen width. 
+    # Notify with blue line
+    cv2.line(img, (int(img.shape[1]/5), 0), (int(img.shape[1]/5), img.shape[0]), (255, 0, 0), 2)
+    cv2.line(img, (int(img.shape[1]*4/5), 0), (int(img.shape[1]*4/5), img.shape[0]), (255, 0, 0), 2)
+
+
+    for (x,y,w,h) in cars:
+        # check if vehicle exit from highway line (left border or right border)
+        # if attached to left border, vehicle is leaving from highway red box
+        # if attached to right border, vehicle is leaving from highway red box
+        # otherwise, vehicle is staying on highway green box
+        if (x < int(img.shape[1]/5)):
+            cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+            pass
+        elif (x > int(img.shape[1]*4/5)):
+            cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+            pass
+        else:
+            cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+            pass   
+    cv2.imshow('YTU-CDTP-HIGHWAY', img)
+
+    # check cars object (x,y,w,h) if exit highway line
+    # if exit highway line, record video
+    """
+    if (len(cars) > 0):
+        # check cars position if exit highway line
+        for (x,y,w,h) in cars:
+            if (y < 100):
+                # record video
+                print("Anomaly detected!")
+                # create video name
+                video_name = "video_" + str(time.time()) + ".mp4"
+                # create video path
+                video_path = os.path.join(os.getcwd(), 'videos', video_name)
+                # create video writer
+                fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+                out = cv2.VideoWriter(video_path, fourcc, 20.0, (640,480))
+                # record video
+                while True:
+                    ret, img = cap.read()
+                    if (type(img) == type(None)):
+                        break
+                    out.write(img)
+                # close video writer
+                out.release()
+                # exit loop
+                break
+            pass
+        pass
+    """
+    if cv2.waitKey(33) == 27:
+        break
+
+cv2.destroyAllWindows()
